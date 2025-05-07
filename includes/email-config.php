b8910033fa73fcedd308b1b9da6b4fb5d70d1004
Shivam Patsariya <s.patsariya@gmail.com>
2025-05-07 00:28:06 -0400

    ssl

--- includes/email-config.php
+++ includes/email-config.php
@@ -4,7 +4,7 @@ define('SMTP_HOST', 'smtp.hostinger.com'); // Replace with actual SMTP host
 define('SMTP_USER', 'no-reply@eazyhaven.com'); // Replace with actual email
 define('SMTP_PASSWORD', 'E@$Y#@ven2025'); // Replace with actual password
 define('SMTP_PORT', 465); // Use 587 for TLS or 465 for SSL
-define('SMTP_SECURE', 'tls'); // Use 'tls' or 'ssl' as required
+define('SMTP_SECURE', 'ssl'); // Use 'tls' or 'ssl' as required
 
 // Email sender information
 define('EMAIL_FROM', 'no-reply@eazyhaven.com');
